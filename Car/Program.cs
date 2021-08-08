d435e146f2a95ba65f9f5c7bc2eaa1342d424bc6
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-08-08 15:06:21 -0500

    testing branch

--- Car/Program.cs
+++ Car/Program.cs
@@ -8,6 +8,9 @@ namespace CarNS
         {
             Car car = new Car("Toyota", "Prius", 10, 50);
             Console.WriteLine(car.Make + " - " + car.Model);
+            Console.WriteLine(car.Make + " ~~ " + car.Model);
+
         }
+
     }
 }
